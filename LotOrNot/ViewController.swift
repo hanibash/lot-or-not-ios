5c30d43a50f39443c268838203aeee09f6b62566
Jordan Ellis <jordane@tophatter.com>
2016-12-16 13:48:22 -0800

    new initial commit

--- LotOrNot/ViewController.swift
+++ LotOrNot/ViewController.swift
@@ -9,11 +9,76 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lotImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dislikeButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let scriptUrl = "http://ec2-54-196-192-180.compute-1.amazonaws.com/products/next.json?access_token=72b132ca963c5ade9499bf32f6c29ad8"
+        // Add one parameter
+        // Create NSURL Ibject
+        let myUrl = NSURL(string: scriptUrl);
+        
+        // Creaste URL Request
+        let request = NSMutableURLRequest(URL:myUrl!);
+        
+        // Set request HTTP method to GET. It could be POST as well
+        request.HTTPMethod = "GET"
+        
+        
+        // Excute HTTP Request
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+            // Check for error
+            if error != nil
+            {
+                print("error=\(error)")
+                return
+            }
+            
+            // Print out response string
+            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print("responseString = \(responseString)")
+            
+            
+            // Convert server json response to NSDictionary
+            do {
+                if let convertedJsonIntoDict = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? NSDictionary {
+                    
+                    // Print out dictionary
+                    print(convertedJsonIntoDict)
+                    
+                    // Get value by key
+                    let imageUrl = convertedJsonIntoDict["primary_image"] as? String
+                    
+                    if let url = NSURL(string: imageUrl!) {
+                        if let data = NSData(contentsOfURL: url) {
+                            self.lotImage.image = UIImage(data: data)
+                            self.lotImage.reloadInputViews()
+                        }
+                    }
+                    
+                    print(imageUrl)
+                    
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+            
+        }
+        
+        task.resume()
+        
+
     }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
